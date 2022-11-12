283bd567389e408ff96bb703270db0856d6bfe17
Dmoraga2193 <d.moraga@duocuc.cl>
2022-11-12 20:25:18 -0300

    Implementacion django
    Se pueden crear usuarios (medicos, pacientes y previsiones de salud) ademas ejecutar "pip install -r requeriments.txt" en consola para instalar lo necesario para ejecutar el proyecto en cualquier pc

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Prevision_Salud,Paciente,Medico
 
 # Register your models here.
+
+
+admin.site.register(Prevision_Salud)
+admin.site.register(Paciente)
+admin.site.register(Medico)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Prevision_Salud(models.Model):
+    nombre = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.nombre
+
+class Paciente(models.Model):
+    nombre = models.CharField(max_length=15)
+    apellido_paterno = models.CharField(max_length=15)
+    apellido_materno = models.CharField(max_length=15)
+    edad = models.IntegerField()
+    fecha_nacimiento = models.DateField()
+    prevision_salud = models.ForeignKey(Prevision_Salud, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.nombre
+
+class Medico(models.Model):
+    nombre = models.CharField(max_length=15)
+    apellido_paterno = models.CharField(max_length=15)
+    apellido_materno = models.CharField(max_length=15)
+    edad = models.IntegerField()
+    fecha_nacimiento = models.DateField()
+    especialidad = models.TextField()
+
+    def __str__(self):
+        return self.nombre
