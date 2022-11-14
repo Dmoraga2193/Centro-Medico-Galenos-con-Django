efbdb4dee57ee7fbf993eb1bc6d5d3896e4178a9
Dmoraga2193 <d.moraga@duocuc.cl>
2022-11-14 02:29:46 -0300

    implementado login y register

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index,contact,about
+from .views import index,contact,about,registro
 
 urlpatterns = [
     path('', index, name="home"),
     path('contact/', contact, name="contact"),
     path('about/', about, name="about"),
+    path('registro/',registro,name="registro"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from .forms import ContactoForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import ContactoForm, CustomUserCreationForm
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
@@ -23,3 +25,19 @@ def contact(request):
 
 def about(request):
     return render(request, 'app/about.html')
+
+def registro(request):
+    data = {
+        'form': CustomUserCreationForm()
+    }
+
+    if request.method == 'POST':
+        formulario = CustomUserCreationForm(data=request.POST)
+        if formulario.is_valid():
+            formulario.save()
+            user = authenticate(username=formulario.cleaned_data["username"],password=formulario.cleaned_data["password1"])
+            login(request, user)
+            messages.success(request, "Te has registrado correctamente")
+            return redirect(to="home")
+        data["form"] = formulario
+    return render(request, 'registration/registro.html', data)
